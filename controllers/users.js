e99900f19aaee2b4d300135f8bfd5ae4c7c32a71
채란 <nemo20435@gmail.com>
2023-01-13 14:09:09 +0900

    user controller 줄바꿈 수정

--- controllers/users.js
+++ controllers/users.js
@@ -18,21 +18,21 @@ module.exports.create = async(req, res) => {
 // find user
 module.exports.find = async (req, res) => {
     try {
-    const { userId } = req.params;
+        const { userId } = req.params;
     
-    // req.params.userId가 있을 시 해당 유저 정보를 리턴한다.
-    if (userId) {
-    // userId가 일치하는 User 도큐먼트를 하나 찾는다.
-    const user = await User.findOne({ userId });
+        // req.params.userId가 있을 시 해당 유저 정보를 리턴한다.
+        if (userId) {
+            // userId가 일치하는 User 도큐먼트를 하나 찾는다.
+            const user = await User.findOne({ userId });
     
-    // 해당 유저 정보가 존재하지 않으면 404를 리턴한다.
-    if (!user) return res.status(404).send("user not found");
-    return res.send(user);
-    }
-    // req.params.userId가 없으면 모든 유저 정보를 리턴한다.
-    const users = await User.find({});
-    return res.send(users);
+            // 해당 유저 정보가 존재하지 않으면 404를 리턴한다.
+            if (!user) return res.status(404).send("user not found");
+            return res.send(user);
+        }
+        // req.params.userId가 없으면 모든 유저 정보를 리턴한다.
+        const users = await User.find({});
+        return res.send(users);
     } catch (err) {
-    return res.status(500).send(err);
+        return res.status(500).send(err);
     }
 };
