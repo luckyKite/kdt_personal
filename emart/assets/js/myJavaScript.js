fbe5528bb9af7b2618017a752a0bb5e99a70c302
채란 <nemo20435@gmail.com>
2023-01-18 16:31:23 +0900

    홍길동으로 출력되지 않는다 (각 함수의 name은 다르다)

--- emart/assets/js/myJavaScript.js
+++ emart/assets/js/myJavaScript.js
@@ -1,11 +1,15 @@
 console.log("잘 되나요?");
 
-function getName(myName) {
-  return myName+"님 안녕하세요";
+let name = '홍길동';
+
+function getName(name) {
+  console.log("이름은"+name+"입니다");
+  return name+"님 안녕하세요";
 }
 
 function test() {
   let name = window.prompt("이름을 입력하세요");
+  console.log("이름은"+name+"입니다");
   document.getElementById("app").innerHTML = 
   `<h1>${getName(name)}</h1>`
 }
