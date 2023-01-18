afa12cd7e17fee4bee45a0874ff9ed64944e9ea5
채란 <nemo20435@gmail.com>
2023-01-18 17:30:31 +0900

    id검색으로 Data에 저장된 사람찾기

--- emart/assets/js/myDatas.js
+++ emart/assets/js/myDatas.js
@@ -20,8 +20,16 @@ console.log(Data[1].name, Data[1].email);
 
 const myFunction =  () => {
 
-  let myName = window.prompt("이름을 입력하세요");
-  Data[0].name = myName;
-  console.log(Data[0].name);
-  document.getElementById("app").innerHTML = `<h1>${Data[0].name}</h1>`;
+  let myId = window.prompt("id를 입력하세요");
+  for(let i in Data) {
+    if(myId == Data[i].id) {
+      console.log(Data[i].name, Data[i].email);
+      document.getElementById("app")
+        .innerHTML = `<h1>${Data[1].name}의 이메일은 ${Data[i].emial}입니다.</h1>`;
+        break;
+    } else {
+      console.log("없는 아이디입니다.");
+    }
+  }
+  
 }
