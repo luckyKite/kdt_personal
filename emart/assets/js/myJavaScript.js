f21ae7ec5564ff72315ba7759d00a35aebb604e8
채란 <nemo20435@gmail.com>
2023-01-18 17:20:08 +0900

    입력한 이름 출력 (기존 데이터와 관계없이)

--- emart/assets/js/myJavaScript.js
+++ emart/assets/js/myJavaScript.js
@@ -1,15 +1,25 @@
 console.log("잘 되나요?");
 
-let name = '홍길동';
+var name = "홍길동";
 
-function getName(name) {
-  console.log("이름은"+name+"입니다");
-  return name+"님 안녕하세요";
-}
 
 function test() {
-  let name = window.prompt("이름을 입력하세요");
-  console.log("이름은"+name+"입니다");
-  document.getElementById("app").innerHTML = 
-  `<h1>${getName(name)}</h1>`
+
+  var name = window.prompt("이름을 입력하세요");
+  console.log("이름은 " + name + "입니다.")
+  document.getElementById("app")
+    .innerHTML = `<h1>${getName(name)}</h1>`;
+
+}
+
+const getName = function(name) {
+
+  var name = name;
+  console.log("이름은 " + name + "입니다.")
+  return name + "님 안녕하세요";
+
+} 
+
+const setName = () => {
+  console.log("test");
 }
