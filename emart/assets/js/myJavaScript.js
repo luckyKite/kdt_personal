d6bb427d352a5dbd87e6b1da83512a2384bea6ee
채란 <nemo20435@gmail.com>
2023-01-18 16:16:54 +0900

    버튼 클릭, 이름 받아와서 인사출력

--- emart/assets/js/myJavaScript.js
+++ emart/assets/js/myJavaScript.js
@@ -1,5 +1,11 @@
 console.log("잘 되나요?");
 
+let myName = "Jason";
+
+function getName() {
+  return myName+"님 안녕하세요";
+}
+
 function test() {
-  alert("Warning!");
+  document.getElementById("app").innerHTML = getName();
 }
