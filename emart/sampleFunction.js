33477198641c22675f97808b977229c35800c5b0
채란 <nemo20435@gmail.com>
2023-01-19 22:40:49 +0900

    list for each

--- emart/sampleFunction.js
+++ emart/sampleFunction.js
@@ -6,6 +6,50 @@ const Data = [
   [21, 22, 23, 24, 25]
 ]
 
+const people = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'email@email.com',
+    item: {
+      id: 1,
+      name: 'item1',
+      price: 1000
+    },
+    go: view = () => {
+      console.log("view");
+    }
+  },
+  {
+    id: 2,
+    name: 'Mary Smith',
+    email: 'mary@email.com'
+  },
+  {
+    id: 3,
+    name: 'Kim Doe',
+    email: 'Kim@email.com'
+  }
+]
+
+
+const myFunction = () => {
+
+  let myId = parseInt(window.prompt("id를 입력하세요"));
+  for(let i in Data) {
+    if(myId === Data[i].id) {
+      console.log(Data[i].name, Data[i].email);
+      document.getElementById("app")
+        .innerHTML = `<h3>${Data[i].name}의 이메일은 ${Data[i].email}입니다.</h3>`;
+      break;
+    } else {
+      console.log("없는 아이디입니다.");
+    }
+
+  }
+  
+}
+
 for(let i = 0; i < 5; i++) {
   let str = "";
   for(let j = 0; j < 5; j++) {
@@ -57,3 +101,20 @@ console.log(txt.trim());
 
 console.log(txt.toUpperCase());
 console.log(txt);
+
+let numb = "https://www.w3schools.com/js/tryit.asp?filename=tryjs_string_padding3";
+let strList = numb.split('/');
+
+console.log(strList);
+
+people.forEach(item => {
+ 
+    let txt = "";
+    txt = `
+      <p>ID: ${item.id}</p>
+      <p>Name: ${item.name}</p>
+      <p>Email: ${item.email}</p>
+      <hr>
+   `;
+   document.getElementById("app").innerHTML += txt;
+});
